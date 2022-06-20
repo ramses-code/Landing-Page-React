29c5957273c1aecb6be2092c257dd77d938aac39
Ramses Cevallos <cevallosramses@gmail.com>
2022-06-20 22:54:40 +0000

    added file cardProps/optimized Cards component

--- src/js/component/cardProp.js
+++ src/js/component/cardProp.js
@@ -1,20 +1,16 @@
 import React from "react"
 import Cards from "./Cards"
 
-let cardProp = {
-    title:['First Img','Second Img', 'Third Img', 'Fourth Img'],
-    button:['First button', 'Second button', 'Third button', 'Fourth button'],
-    text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, porro!',
-    img1:'https://images.unsplash.com/photo-1535905557558-afc4877a26fc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
-}
+let cardPorp = [
+    {title: 'First img', button: 'First button', text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, porro!', img: 'https://images.unsplash.com/photo-1535905557558-afc4877a26fc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'},
+    {title: 'Second img', button: 'Second button', text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, est!', img:  'https://images.unsplash.com/photo-1529148482759-b35b25c5f217?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'},
+    {title: 'Third img', button: 'Third button', text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, quas.', img: 'https://images.unsplash.com/photo-1532012197267-da84d127e765?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGxpYnJhcnl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'},
+    {title: 'Fourth img', button: 'Fourth button', text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque, quod.', img: 'https://images.unsplash.com/photo-1555116505-38ab61800975?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGxpYnJhcnl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'}
+];
 
-let titles = cardProp.title.map(ele => {
-    let n = <Cards title={ele}/>
-    return n
-})
-let buttons = cardProp.button.map(ele => {
-    let n = <Cards button={ele}/>
-    return n
+let cardsProperties = cardPorp.map(ele => {
+    return  <Cards title={ele.title} button={ele.button} text={ele.text} img={ele.img} />
 })
 
-export {titles}
+
+export {cardsProperties}
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,23 +1,12 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Jumbotron from "./Jumbotron";
-import Cards from "./Cards";
 import Footer from "./Footer";
-import {titles} from "./cardProp";
+import {cardsProperties} from "./cardProp";
 //include images into your bundle
 
 //create your first component
 const Home = () => {
-
-	let cardPorp = {
-		title:['First Img','Second Img', 'Third Img', 'Fourth Img'],
-		button:['First button', 'Second button', 'Third button', 'Fourth button'],
-		text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, porro!',
-		img:['https://images.unsplash.com/photo-1535905557558-afc4877a26fc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
-			'https://images.unsplash.com/photo-1529148482759-b35b25c5f217?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-			'https://images.unsplash.com/photo-1532012197267-da84d127e765?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGxpYnJhcnl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-			'https://images.unsplash.com/photo-1555116505-38ab61800975?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGxpYnJhcnl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60']
-	}
 
 	return (
 		<div className="container-fluid px-0">
@@ -25,10 +14,7 @@ const Home = () => {
 			<div className="container">				
 				<Jumbotron/>
 				<div className='row mx-1 mb-2'>
-					<Cards title={cardPorp.title[0]} button={cardPorp.button[0]} text={cardPorp.text} img={cardPorp.img[0]}/>
-					<Cards title={cardPorp.title[1]} button={cardPorp.button[1]} text={cardPorp.text} img={cardPorp.img[1]}/>
-					<Cards title={cardPorp.title[2]} button={cardPorp.button[2]} text={cardPorp.text} img={cardPorp.img[2]}/>
-					<Cards title={cardPorp.title[3]} button={cardPorp.button[3]} text={cardPorp.text} img={cardPorp.img[3]}/>
+					{cardsProperties}
 				</div>
 			</div>
 			<Footer/>
